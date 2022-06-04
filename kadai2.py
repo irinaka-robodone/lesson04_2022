68b624821e53c268f74f1d408c7b6e0444767b58
Irinaka Robodone <irinaka.robodone@gmail.com>
2022-06-04 19:38:48 +0900

    init commit

---
+++ kadai2.py
@@ -0,0 +1,8 @@
+print("５＊２＝？")
+
+kotae = input()
+
+if kotae == "10":
+    print("正解")
+else:
+    print("不正解")
---
+++ main.py
@@ -0,0 +1,8 @@
+print("今日の天気は")
+
+kotae = input()
+
+if kotae == "晴れ":
+    print("遊ぼう")
+else kotae == "雨"：
+    print("ゲームだ")
